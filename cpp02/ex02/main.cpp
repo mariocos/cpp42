52fb2fd2e22cad9dd8ad8d4415f6c5051034e03c
mariocos <alpacasrcool7@gmail.com>
2025-02-13 11:45:15 +0000

    adding the cpp02

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -1,15 +1,15 @@
 #include "fixed.hpp"
 
 int main( void ) {
-	//fixed a;
+	fixed a;
 	fixed const	b(fixed(5.05f) * fixed(2));
-	/* std::cout << b << "b\n";
+	std::cout << b << "b\n";
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;*/
+	std::cout << a << std::endl;
 	std::cout << b << std::endl;
-//	std::cout << fixed::max(a, b) << std::endl; */
+	std::cout << fixed::max(a, b) << std::endl;
 	return 0;
 }
